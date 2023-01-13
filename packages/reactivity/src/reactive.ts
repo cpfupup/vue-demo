247ebaec774431d967e23f9157fef8b4ac14a6b5
asherchen(陈芃霏) <asherchen@futunn.com>
2023-01-13 15:57:28 +0800

    通过用两个weakmap代理对象

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -24,13 +24,16 @@ export function shallowReadonly(target) {//浅只读
 
 
 // 柯里化处理共用部分 new Proxy() 最核心的需要拦截数据的读取和数据的修改 get set
+const reactiveMap = new WeakMap();//会自动垃圾回收，不会造成内存泄漏，存储的key只能是对象
+const readonlyMap = new WeakMap()
 export function createReactiveObjet(target, isReadonly, baseHandlers) {
   // 如果目标不是对象 无法拦截， reactive这个api只能拦截对象类型
   if (!isObject(target)) {
     return target//如果不是对象直接返回
   }
-  
+
   // 如果某个对象已经被代理过了，就不需要再次代理 可能一个对象 被代理是深度 又被仅读代理了
+  const proxyMap = isReadonly ? readonlyMap : reactiveMap//如果是只读就由只读来代理
   const proxy = new Proxy(target, baseHandlers)
   return proxy
 }
