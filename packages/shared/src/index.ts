7207aeb319af4e13ef4b9b45363f8983425ff280
asherchen(陈芃霏) <asherchen@futunn.com>
2023-01-16 10:36:41 +0800

    新增了对象合并方法

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,1 +1,2 @@
-export const isObject = (value) => typeof value === 'object' && value !== null //判断是否为对象且不为空
+export const isObject = (value) => typeof value === 'object' && value !== null // 判断是否为对象且不为空
+export const extend = Object.assign // Object.assign方法用于对象的合并，将源对象（ source ）的所有可枚举属性，复制到目标对象（ target ）。
