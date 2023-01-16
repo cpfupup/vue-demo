09aae6961eaf02a60d464c496b56d213337535ee
asherchen(陈芃霏) <asherchen@futunn.com>
2023-01-16 14:18:24 +0800

    实现响应逻辑基本getset

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,5 +1,47 @@
-//定义方法
-export const mutableHandlers = {}
-export const shallowReactiveHandlers = {}
-export const readonlyHanlers = {}
-export const shallowReadonlyHandlers = {}
+// 响应式逻辑
+// 是否为仅读， 仅读属性set时会异常
+// 是否为深度
+
+import { extend } from "@vue/shared"
+
+// 抽离出get函数 判断是否为仅读 是否为浅读
+function createGetter(isReadonly = false, shallow = false) {
+
+}
+
+function createSetter(isReadonly = false) {
+
+}
+
+const get = createGetter()
+const shallowGet = createGetter(false, true)
+const readonlyGet = createGetter(true)
+const shallowrReadonlyget = createGetter(true, true)
+const set = createSetter();
+const shallowSet = createSetter(true)
+
+// 可写属性，需要考虑set是否为浅修改
+export const mutableHandlers = {
+  get: get,
+  set
+}
+
+export const shallowReactiveHandlers = {
+  get: shallowGet,
+  shallowSet
+}
+
+// 抽离只读属性的set
+let readonlyObj = {
+  set: (target, key) => {
+    console.warn(`set ${target}on key ${key}failed`);
+  }
+}
+// 只读属性 不需要考虑set问题
+export const readonlyHanlers = extend({
+  get: readonlyGet,
+}, readonlyObj)
+
+export const shallowReadonlyHandlers = extend({
+  get: shallowrReadonlyget
+}, readonlyObj)
