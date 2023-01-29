ddc5409c6650c71f9b532e4fcd91b4e03b856df7
asherchen(陈芃霏) <asherchen@futunn.com>
2023-01-29 16:34:32 +0800

    完善effect函数

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -4,6 +4,7 @@
 
 import { extend, isObject } from "@vue/shared"
 import { track } from "./effect"
+import { TrackOptypes } from "./operators"
 import { reactive, readonly } from "./reactive"
 
 
@@ -43,7 +44,7 @@ function createGetter(isReadonly = false, shallow = false) {// 拦截获取功�
 
     if (!isReadonly) {
       // 收集依赖，数据变化后更新对应的视图
-      track(target, key)// 收集对象和具体的属性
+      track(target, TrackOptypes.GET, key)// 收集对象，对象的操作和具体的属性
       console.log('执行effect时会取值', '收集effect');
 
     }
--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -11,16 +11,45 @@ export function effect(fn, options: any = {}) {// 第一个参数为要运行的
 }
 
 let uid = 0;// effect的唯一标识
+let activeEffect// 存储当前的effect
+const effectStack = []// 用栈出入保证当前effect是正确的顺序
+
 function creatReactiveEffect(fn, options) {
   const effect = function reactiveEffect() {
-    fn()// 函数执行时会取值 会执行get方法
+    if (!effectStack.includes(effect)) { // 保证effect没有加入到effectStack中
+      try {
+        effectStack.push(effect)
+        activeEffect = effect;
+        fn()// 函数执行时会取值 会执行get方法
+      } finally {
+        effectStack.pop()// 出栈
+        activeEffect = effectStack[effectStack.length - 1]// 指针指向前一个effect
+      }
+    }
   }
   effect.id = uid++; // 制作一个effect标识，用于区分effect
   effect._isEffect = true;// 用于标识这个是响应式effect 
   effect.raw = fn;// 记录对应的原函数
-  effect.options = options //保存用户的属性
+  effect.options = options //保存用户的属性 
   return effect;
 }
-export function track(target, key) {
+const targetMap = new WeakMap();
+//让某个对象中的属性 收集他当前对应的effect函数
+export function track(target, type, key) {
+  // activeEffect // 可以拿到当前的effect
 
+  if (activeEffect === undefined) { // 此属性不需要收集依赖，因为没在effect中使用
+    return;
+  }
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map))
+  }
+  let dep = depsMap.get(key);
+  if (!dep) {
+    depsMap.set(key, dep = new Set)
+  }
+  if (!dep.has(activeEffect)) {
+    dep.add(activeEffect)
+  }
 }
