341740df639d4c8eb8f0c54f88361d9247bae97b
asherchen(陈芃霏) <asherchen@futunn.com>
2023-02-17 16:45:37 +0800

    抽离渲染器

--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -4,26 +4,12 @@
 import { extend } from "@vue/shared";
 import { nodeOps } from "./nodeOps";  //对象
 import { pathProp } from "./pathProp"; //方法
-
+import { createRenderer } from '@vue/runtime-core';
 // 渲染时用到的所有方法
 const rendererOptions = extend({ pathProp }, nodeOps)
 
 //用户调用的是runtime-dom -> runtime-core
 //run time-dom 是为了解决平台差异 （浏览器端）
-
-function createRenderer(rendererOptions) { //告诉core 怎么渲染
-  return {
-    createApp(rootComponent, rootProps) { //告诉他哪个组件 哪个属性来创建的应用
-      const app = {
-        mount(container) {
-          console.log(container,rootComponent,rootProps,rendererOptions);//渲染容器 组件 属性 方法
-        }
-      }
-      return app
-    }
-  }
-}
-
 //vue中的runtime-core提供了核心的方法 用来处理渲染 他会使用runtime-dom中的api进行渲染
 export function createApp(rootComponent, rootProps = null) {
   const app: any = createRenderer(rendererOptions).createApp(rootComponent, rootProps)
@@ -32,7 +18,7 @@ export function createApp(rootComponent, rootProps = null) {
   app.mount = function (container) {
     container = nodeOps.querySelector(container)
     container.innerHTML = ''
-    mount(container)
+    mount(container) //函数劫持
     //将组建渲染成dom元素进行挂载
   }
   return app
