d8dcf20cb0b4cab29a2736f802da9ed7e537ac03
asherchen(陈芃霏) <asherchen@futunn.com>
2023-02-20 17:00:36 +0800

    补充mountComponent流程

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -1,9 +1,22 @@
 import { shapeFlags } from "packages/shared/src/shapeFlag";
 import { createAppAPI } from "./apiCreateApp"
+import { createComponentInstance, setupComponent } from "./component";
 
 export function createRenderer(rendererOptions) { //告诉core 怎么渲染
+
   const mountComponent = (initialVNode, container) => { //初始化节点的方法
     //组件的渲染流程 最核心的就是调用 setup拿到返回值 获得render函数返回的结果来进行渲染
+    const retupRunderEfect = () => {
+
+    }
+    //1.先有实例
+    const instance = initialVNode.component = createComponentInstance(initialVNode) //通过createComponent来创建实例挂载到initialVNode.component上
+
+    // 2.需要的数据解析到实例上
+    setupComponent(instance)
+    // 3.创建一个effect 让render函数执行
+
+    retupRunderEfect()
   }
   const processComponent = (n1, n2, container) => {
     if (n1 == null) { //组件没有上一次的虚拟节点 进行初始化
